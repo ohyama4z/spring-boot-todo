3305d0741f8bf8fcf2c84aef11d27f76de49fe3b
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2022-02-21 01:29:12 +0900

    [add] entityとrepositoryの作成

---
+++ src/main/java/com/example/springboottodo/entity/Todo.java
@@ -0,0 +1,16 @@
+package com.example.springboottodo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "todo_item")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private boolean done;
+}
